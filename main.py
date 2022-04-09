3ad6c0e0bc0d017ea0cdc4f11d2f7f1ba66495ed
maxburgert <max.burgert@tum.de>
2022-04-09 19:48:34 +0200

    Adds second website to monitor

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
-url = 'https://standert.de/collections/triebwerk-disc/products/triebwerk-disc-ltd-2022-frame?variant=42550256959701'
+urls = list(['https://standert.de/collections/triebwerk-disc/products/triebwerk-disc-ltd-2022-frame?variant=42550256959701',
+           'https://standert.de/collections/triebwerk-disc/products/triebwerk-disc-ltd-2022?variant=42550325280981'])
 
 
 def main():
@@ -12,24 +13,25 @@ def main():
                         filemode='w', level=logging.INFO, encoding='utf-8')
 
     logging.info('-- Start Availability Check --')
-    page = urllib.request.urlopen(url)
-    logging.info(f'Page status code: {page.status}')
-    if page.status != 200:
-        logging.exception(f'Page status code not 200 at {url}')
-        return
+    for url in urls:
+        page = urllib.request.urlopen(url)
+        logging.info(f'Page status code: {page.status}')
+        if page.status != 200:
+            logging.exception(f'Page status code not 200 at {url}')
+            return
 
-    soup = BeautifulSoup(page, features='html.parser')
-    # get contents of div
-    result = soup.find_all("div", {"class": "availability"})
-    content = result[0].text
-    logging.info(content.rstrip('\n'))
+        soup = BeautifulSoup(page, features='html.parser')
+        # get contents of div
+        result = soup.find_all("div", {"class": "availability"})
+        content = result[0].text
+        logging.info(content.rstrip('\n'))
 
-    if '54: Out of stock.' not in content:
-        logging.info('Available')
-        exit(-1)
-    else:
-        logging.info('Not available')
-        #exit(-1)
+        if '54: Out of stock.' not in content:
+            logging.info('Available')
+            exit(-1)
+        else:
+            logging.info('Not available')
+            #exit(-1)
 
 
 if __name__ == '__main__':
